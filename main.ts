b98e81e0522c40ff675c58f2f263d925dfee8035
Ashlyn-Anpalahan <ashlyn.anpalahan@stu.ocsb.ca>
2026-02-11 13:10:47 -0500

    Changed comment

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ashlyn
+ * Created on: Feb 2026
+ * This program shows my name
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, Ashlyn!')
